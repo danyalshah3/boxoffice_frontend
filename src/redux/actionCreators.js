e39f9273d54de4c9e132fd6a15eb88e7c219575e
danyalshah3 <72283767+danyalshah3@users.noreply.github.com>
2021-11-12 17:12:34 -0500

    added selected movies state

--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -4,3 +4,11 @@ export const getMovies = () => {
     .then(res => res.json())
     .then(movies => dispatch({type: 'GET_MOVIES', payload: movies}))
 }
+
+
+export const getMovie = (id) => {
+  
+    return dispatch => fetch(`http://localhost:3000/movies/${id}`)
+    .then(res => res.json())
+    .then(movie => dispatch({type: 'GET_MOVIE', payload: movie}))
+}
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,5 +1,22 @@
 const initialState = {
-    movies: []
+    movies: [],
+    selectedMovie: {
+        title: "",
+        released: "",
+        genre: "",
+        posterImg: "",
+        imdbRating: 0,
+        runtime: "",
+        director: "",
+        writer: "",
+        actor: "",
+        plot: "",
+        language: "",
+        awards: "",
+        boxOffice: "0",
+        trailer: ""
+      
+    }
 }
 
 
@@ -8,6 +25,9 @@ export default function reducer(state=initialState, action) {
     switch (action.type){
     case "GET_MOVIES":
         return {...state, movies: action.payload}
+
+        case "GET_MOVIE":
+            return {...state, selectedMovie: action.payload}
         default: 
         return {...state}
      
