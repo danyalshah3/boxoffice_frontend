34ebe417aab1e3fda633dcd4ffa1a6c565b37aa0
danyalshah3 <72283767+danyalshah3@users.noreply.github.com>
2021-11-12 12:45:28 -0500

    fetch inside actioncreators

--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -1,6 +1,6 @@
-// export const getMovies = () => {
+export const getMovies = () => {
   
-//     return dispatch => fetch("http://localhost:3000/movies")
-//     .then(res => res.json())
-//     .then(movies => dispatch({type: 'GET_MOVIES', payload: movies}))
-// }
+    return dispatch => fetch("http://localhost:3000/movies")
+    .then(res => res.json())
+    .then(movies => dispatch({type: 'GET_MOVIES', payload: movies}))
+}
