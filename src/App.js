d8ef1cf5abdeb826a0a7f08ec4519331c02f315b
danyalshah3 <72283767+danyalshah3@users.noreply.github.com>
2021-11-12 16:18:17 -0500

    added initial index route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import MovieIndex from './containers/MovieIndex'
+import {Switch, Route} from 'react-router-dom'
 
 
 function App() {
   return (
     <>
  <h1>Movies</h1>
-    <MovieIndex />
+    <Switch>
+      <Route path="/restaurants/:id"></Route>
+    <Route path="/movies"><MovieIndex /></Route>
+      </Switch>
      </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import {createStore, applyMiddleware, compose} from 'redux'
 import reducer from './redux/reducer'
 import {Provider} from 'react-redux'
 import thunk from 'redux-thunk'
+import {BrowserRouter as Router} from 'react-router-dom'
+
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, composeEnhancers( applyMiddleware(thunk)
@@ -14,7 +16,7 @@ const store = createStore(reducer, composeEnhancers( applyMiddleware(thunk)
 
 ReactDOM.render(
   <React.StrictMode>
-   <Provider store={store}> <App /></Provider> 
+   <Router><Provider store={store}> <App /></Provider></Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
