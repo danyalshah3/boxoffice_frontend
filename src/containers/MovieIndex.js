0f09a08a126213fca3b73b61330967c5510e7870
danyalshah3 <72283767+danyalshah3@users.noreply.github.com>
2021-11-12 12:44:08 -0500

    added connect

--- src/containers/MovieIndex.js
+++ src/containers/MovieIndex.js
@@ -2,18 +2,18 @@ import {useEffect} from 'react'
 import {getMovies} from '../redux/actionCreators'
 import {connect} from  'react-redux'
 
-function MovieIndex({getMovies, movies}) {
+// function MovieIndex({getMovies, movies}) {
 
 
-    useEffect(() => movies.length === 0 && getMovies(), [movies])
-    // console.log(props.movies)
+//     useEffect(() => movies.length === 0 && getMovies(), [movies])
+//     // console.log(props.movies)
 
-    return <h1> Movies go here </h1>
+//     return <h1> Movies go here </h1>
 
     
 
 
-}
+// }
 
 const mapStateToProps = (state) => {
   return {movies: state.movies}  
